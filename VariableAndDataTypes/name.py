8a9a0c86780500499ca86ca79becb1bb0da2bf4b
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-03 00:51:00 +0530

    Learning something new

--- VariableAndDataTypes/name.py
+++ VariableAndDataTypes/name.py
@@ -41,4 +41,61 @@
 # Python
 #         Python
 
-print("Languages:\nPython\nC\nJavaScript")
+# print("Languages:\nPython\nC\nJavaScript")
+# Output
+# Languages:
+# Python
+# C
+# JavaScript
+
+
+# print("Languages:\n\tPython\n\tC\n\tJavaScript")
+#Output
+# Languages:
+#         Python
+#         C
+#         JavaScript
+
+
+# Stripping Whitespaces
+# favorite_language = 'python '
+# print(favorite_language)
+# print(favorite_language.rstrip())
+
+
+
+# favorite_language = 'python '
+# favorite_language = favorite_language.rstrip()
+# print(favorite_language)
+
+# favorite_language = ' python '
+# print(favorite_language.rstrip())
+# print(favorite_language.lstrip())
+# print(favorite_language.strip())
+
+
+# Avoiding Syntax Errors with Strings
+# message = "One of Pyton's strenghts is its diverse community."
+# print(message)
+
+
+# Exersice 1.3
+person_name = "Ahsan"
+print(f"Hello {person_name}, would you like to learn some Python todqy?")
+
+# Exersice 1.4
+person_name = "Asad"
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
+
+# Exersice 1.5
+famous_person = "Albert Einstein"
+Quote = "A person who never made a mistake never tried anyting new."
+print(f"{famous_person} once said, {Quote}")
+
+# Exersice 1.6
+person_name = '\t\n Ahsans \n\t'
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
