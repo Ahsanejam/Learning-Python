882d5abfef7356ad280aa1081a8a6da2cad8666e
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-03 00:27:51 +0530

    Adding new files

---
+++ VariableAndDataTypes/Basic.py
@@ -0,0 +1,21 @@
+# Variables 
+# message = "Hello Python World!"
+# print(message)
+
+# message = "Hello Python Crash Course world!"
+# print(message)
+
+# message = "Hello Python Crash Course reader!"
+# print(message)
+
+
+# Exersice 1.1
+message = "Hello World"
+print(message)
+
+# Exersice 1.2
+message = "Hello World I am waiting for you"
+print(message)
+
+
+
---
+++ VariableAndDataTypes/name.py
@@ -0,0 +1,38 @@
+# Strings
+ 
+# name = "ada lovelace"
+# print(name.title())
+
+# Output -> Ada Lovelace
+
+# name = "Ada Lovelace"
+# print(name.upper())
+# print(name.lower())
+
+# Output----
+# ADA LOVELACE
+# ada lovelace
+
+
+# Using Variable in Strings
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+
+# Output -> ada lovelace
+
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(full_name.title())
+
+# Output -> Ada Lovelace
+
+
+# Adding Whitespace to Strings with Tabs or Newlines
+
+print("Python")
+print("\tPython")
+
