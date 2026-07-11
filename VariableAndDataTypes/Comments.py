1126bc0fc0cf4d490a48ff66152bff4726b28c97
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-12 02:29:56 +0530

    Learn about numbers and floats

---
+++ VariableAndDataTypes/Comments.py
@@ -0,0 +1,15 @@
+# Say hello to everyone.
+
+print("Helo Python people!")
+
+
+
+# Exersice 1.10
+message = "Hello World"
+# This message is a variable to print hello World and if we print them the output is hello world 
+print(message)
+
+# Exersice 1.2
+message = "Hello World I am waiting for you"
+# Basically we override the message variable to print new message  
+print(message)
---
+++ VariableAndDataTypes/Numbers.py
@@ -0,0 +1,65 @@
+# print(2 + 3)
+# print(3 - 2)
+# print(2 * 3)
+# print(3 / 2)
+
+# # Power
+# print(3 ** 2)
+# print(3 ** 3)
+# print(10 ** 6)
+
+
+# print(2 + 3*4)
+
+# print((2 + 3) * 4)
+
+
+# Floats
+
+
+# print(0.1 + 0.1)
+# print(0.2 + 0.2)
+# print(2 + 0.1)
+# print(2 + 0.2)
+
+
+# print(0.2 + 0.1)
+# print(3 + 0.1)
+
+
+
+
+# Integers and Floats
+# print(4 / 2)
+# print(1 + 2.0)
+# print(2 * 3.0)
+# print(3.0 ** 2)
+
+
+
+# Underscores in Numbers
+
+# universe_age = 14_000_000_000
+# print(universe_age)
+
+
+# Multiple Assignment
+# x, y, z = 1, 3, 6
+# print(f"{x} {y} {z}")
+
+
+# Constants 
+# MAX_CONNECTIONS = 5000
+
+
+# Exersice 1.8
+print(5 + 3)
+print(11 - 3)
+print(4 * 2)
+print(16 / 2)
+
+# Exersice 1.9
+number = 15
+favoriate_number = f"My Favorite number is = {number}"
+print(favoriate_number)
+
