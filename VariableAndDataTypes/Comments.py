7f5d8c3f70245b00433ff1acf2f9558e8a845d93
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-12 02:32:49 +0530

    Learn about import statement

--- VariableAndDataTypes/Comments.py
+++ VariableAndDataTypes/Comments.py
@@ -1,15 +1,19 @@
 # Say hello to everyone.
 
-print("Helo Python people!")
+# print("Helo Python people!")
 
 
 
 # Exersice 1.10
-message = "Hello World"
-# This message is a variable to print hello World and if we print them the output is hello world 
-print(message)
+# message = "Hello World"
+# # This message is a variable to print hello World and if we print them the output is hello world 
+# print(message)
 
-# Exersice 1.2
-message = "Hello World I am waiting for you"
-# Basically we override the message variable to print new message  
-print(message)
+# # Exersice 1.2
+# message = "Hello World I am waiting for you"
+# # Basically we override the message variable to print new message  
+# print(message)
+
+# This is basically to import the rules that are prebuilt in python
+import this
+print(this)
