46343ecdeeba36e65f2be2a71a98a35a3c7481eb
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-12 02:46:06 +0530

    Learn about lists

---
+++ Introducing to Lists/bicycles.py
@@ -0,0 +1,53 @@
+# Lists
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+
+
+
+# Accessing Elements in a List
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[0])
+
+
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[0].title())
+
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[1])
+# print(bicycles[3])
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[-1])
+
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# message = f"My first bicycle was a {bicycles[0].title()}."
+
+# print(message)
+
+
+
+# Exersice 2.1
+# names = ['asad', 'neyaj', 'zafar', 'anand']
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+
+# Exersice 2.2
+# names = ['asad', 'neyaj', 'zafar', 'anand']
+# print(f"Hello {names[0]}, I hope you are fine!")
+
+# print(f"Hello {names[1]}, I hope you are fine!")
+# print(f"Hello {names[2]}, I hope you are fine!")
+# print(f"Hello {names[3]}, I hope you are fine!")
+
+
+Cars = ['bmw', 'mercedies', 'hundai', 'ferari']
+print(f"I would like to own a {Cars[0].title()} car")
+print(f"I would like to own a {Cars[1].title()} car")
+print(f"I would like to own a {Cars[2].title()} car")
+print(f"I would like to own a {Cars[3].title()} car")
+
