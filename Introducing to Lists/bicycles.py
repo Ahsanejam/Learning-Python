fae792305e9aff62ed519a43045732a6714ce70e
Ahsan Ejam <ahsanejam48@gmail.com>
2026-07-13 00:41:32 +0530

    Learn about sorting find length and manipulating lists

--- Introducing to Lists/bicycles.py
+++ Introducing to Lists/bicycles.py
@@ -44,10 +44,162 @@
 # print(f"Hello {names[2]}, I hope you are fine!")
 # print(f"Hello {names[3]}, I hope you are fine!")
 
+# Exersice 2.3
+# Cars = ['bmw', 'mercedies', 'hundai', 'ferari']
+# print(f"I would like to own a {Cars[0].title()} car")
+# print(f"I would like to own a {Cars[1].title()} car")
+# print(f"I would like to own a {Cars[2].title()} car")
+# print(f"I would like to own a {Cars[3].title()} car")
 
-Cars = ['bmw', 'mercedies', 'hundai', 'ferari']
-print(f"I would like to own a {Cars[0].title()} car")
-print(f"I would like to own a {Cars[1].title()} car")
-print(f"I would like to own a {Cars[2].title()} car")
-print(f"I would like to own a {Cars[3].title()} car")
+
+
+# Changing, Adding, and Removing Elements
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+
+
+# Adding Elements to a Lise
+
+# Appending Elements to the End of a List
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# motorcycles.append('ducati')
+# print(motorcycles)
+
+
+# motorcycles = []
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
+
+# print(motorcycles)
+
+
+# Inseting Elements into a List
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# motorcycles.insert(0, 'ducati')
+
+# print(motorcycles)
+
+
+
+
+# Removing Elements from a Lise
+# Removing an item Using the del Statement
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# del motorcycles[0]
+# print(motorcycles)
+
+
+# motorcycles = ['honda', 'yamha', 'suzuki']
+# print(motorcycles)
+
+# del motorcycles[1]
+# print(motorcycles)
+
+
+
+# Removing an item Using the pop() Method
+# motorcycles = ['honda', 'yamha', 'suzuki']
+# print(motorcycles)
+
+# popped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
+
+# motorcycles = ['honda', 'yamha', 'suzuki']
+
+# last_owned = motorcycles.pop()
+# print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+
+
+# Popping Items from any Posiion in a List
+
+# motorcycles = ['honda', 'yamha', 'suzuki']
+
+# first_owned = motorcycles.pop(0)
+# print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+
+# Removing an Item by Value
+
+# motorcycles = ['honda', 'yamha', 'suzuki', 'ducati']
+# print(motorcycles)
+
+# motorcycles.remove('ducati')
+# print(motorcycles)
+
+
+# motorcycles = ['honda', 'yamha', 'suzuki', 'ducati']
+# print(motorcycles)
+
+# too_expensive = 'ducati'
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
+
+# Exersice 2.4
+# guests = ['albert einstein', 'marie curie', 'tesla']
+
+# print(f"Hello {guests[0].title()} I will invite you to today's dinner party")
+# print(f"Hello {guests[1].title()} I will invite you to today's dinner party")
+# print(f"Hello {guests[2].title()} I will invite you to today's dinner party")
+
+
+# Exersice 2.5
+guests = ['albert einstein', 'marie curie', 'tesla']
+print(f"Hello {guests[0].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[1].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[2].title()} I will invite you to today's dinner party")
+
+print(f"You know {guests[2].title()} cannot come to this party")
+print("\n")
+guests[2] = 'musk'
+print(f"Hello {guests[0].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[1].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[2].title()} I will invite you to today's dinner party")
+
+print("\n")
+print("I have a good news you know i found a bid table in today's dinner party so \n I invite three more perons for today's dinner party")
+print('\n')
+
+# Exersice 2.6
+guests.insert(0, 'asad')
+guests.insert(3, 'donald')
+guests.append('anand')
+
+print(guests)
+print(f"Hello {guests[0].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[1].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[2].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[3].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[4].title()} I will invite you to today's dinner party")
+print(f"Hello {guests[5].title()} I will invite you to today's dinner party")
+
+# Exersice 2.7
+print("\n")
+print("Sorry you know i figure out that our dinner table cannot come at time so i have only places for two guests")
+
+print(f"{guests.pop().title()} sorry i feel bad for you to not invite in today's dinner")
+print(f"{guests.pop().title()} sorry i feel bad for you to not invite in today's dinner")
+print(f"{guests.pop().title()} sorry i feel bad for you to not invite in today's dinner")
+print(f"{guests.pop().title()} sorry i feel bad for you to not invite in today's dinner")
+
+
+del guests[0]
+del guests[0]
+print(guests)
 
---
+++ Introducing to Lists/cars.py
@@ -0,0 +1,86 @@
+# Organizing a List
+
+# Sorting a Lise Permanently with the sort()n Method
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+
+
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort(reverse=True)
+# print(cars)
+
+
+# Sorting a List Temporarily with the sorted() Function
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print("Here is the original list: ")
+# print(cars)
+
+# print("\nHere is the sorted list: ")
+# print(sorted(cars))
+
+# print("\nHere is the original list again: ")
+# print(cars)
+
+# Printing a Lise in Reverse Order
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# print(cars)
+
+# cars.reverse()
+# print(cars)
+
+
+
+# Finding the Length of a Lise
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(len(cars))
+
+
+
+
+# Exersice 2.8
+# places = ['spain', 'france', 'italy', 'germany']
+# print("Original list: ")
+# print(places)
+
+# print("\nSorted list: ")
+# print(sorted(places))
+
+# print("\nOriginal list: ")
+# print(places)
+
+# print("\nReverse order: ")
+# print(sorted(places, reverse=True))
+
+# print("\nOriginal list: ")
+# print(places)
+
+# print("\nReverse order: ")
+# places.reverse()
+# print(places)
+
+# print("\n Original form: ")
+# places.reverse()
+# print(places)
+
+# print("\n Sorted Alphabatical order: ")
+# places.sort()
+# print(places)
+
+# print("\n Reverse Alphabatical order: ")
+# places.sort(reverse=True)
+# print(places)
+
+
+
+# Exserice 2.9
+guests = ['albert einstein', 'marie curie', 'tesla']
+
+print(f"Number of guests inviting on dinner is: {len(guests)}")
+
